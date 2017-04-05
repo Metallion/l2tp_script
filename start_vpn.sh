b9f0b7d2b22e9822448ddfc6105bc14f656e1c44
Metallion <akieckens@axsh.net>
2017-04-05 17:00:41 +0900

    A little bit of refactoring and a flag to disable config file backups

--- start_vpn.sh
+++ start_vpn.sh
@@ -6,8 +6,10 @@ if [ -n "$1" ]; then
   source "$1"
 fi
 
-#VPN_SERVER_IP
+#L2TP_SERVER_IP
 #TARGET_IP=
+
+BACKUP_CONFIG_FILES="${BACKUP_CONFIG_FILES:-true}"
 
 TUNNEL_INTERFACE="${TUNNEL_INTERFACE:-ppp0}"
 CONNECTION_NAME="${CONNECTION_NAME:-L2TP-PSK}"
@@ -26,12 +28,20 @@ function get_tunnel_ip() {
   ip addr show $TUNNEL_INTERFACE | grep "inet\b" | awk '{print $2}'
 }
 
+function backup_file() {
+  local file="$1"
+
+  if [[ "${BACKUP_CONFIG_FILES}" == "true"  && -f "${file}" ]]; then
+    cp ${file} ${file}.$(date -Is)
+  fi
+}
+
 LOCAL_INTERFACE="${LOCAL_INTERFACE:-$(get_default_gw_interface)}"
 
 #ip route add $TARGET_IP via $(get_tunnel_ip)
 
 # Create the ipsec config file
-cp ${IPSEC_CONFIG} ${IPSEC_CONFIG}.$(date -Is)
+backup_file ${IPSEC_CONFIG}
 cat > ${IPSEC_CONFIG} << EOS
 config setup
 	virtual_private=%v4:10.0.0.0/8,%v4:192.168.0.0/16,%v4:172.16.0.0/12
@@ -53,27 +63,31 @@ conn ${CONNECTION_NAME}
 	type=transport
 	left=%defaultroute
 	leftprotoport=17/1701
-	right=${VPN_SERVER_IP}
+	right=${L2TP_SERVER_IP}
 	rightprotoport=17/1701
 EOS
 
 # Add the ipsec secrets if not there already
-secret="0.0.0.0 : PSK \"${PRE_SHARED_KEY}\""
-set +e; grep -q "${secret}" "${IPSEC_SECRETS}"
+backup_file ${IPSEC_SECRETS}
+secret="0.0.0.0 : PSK \"${IPSEC_PRE_SHARED_KEY}\""
+set +e
+[[ -f "${IPSEC_SECRETS}" ]] && grep -q "${secret}" "${IPSEC_SECRETS}"
 if [[ "$?" != "0" ]]; then
   echo "${secret}" >> "${IPSEC_SECRETS}"
 fi
 set -e
 
 # Set up xl2tpd
+backup_file ${XL2TPD_CONFIG}
 cat > ${XL2TPD_CONFIG} << EOS
 [lac vpn-connection]
-lns = ${VPN_SERVER_IP}
+lns = ${L2TP_SERVER_IP}
 ppp debug = yes
 pppoptfile = /etc/ppp/options.l2tpd.client
 length bit = yes
 EOS
 
+backup_file ${XL2TPD_CLIENT}
 cat > ${XL2TPD_CLIENT} << EOS
 ipcp-accept-local
 ipcp-accept-remote
@@ -88,6 +102,6 @@ defaultroute
 usepeerdns
 debug
 connect-delay 5000
-name ${VPN_USER_NAME}
-password ${VPN_PASSWORD}
+name ${L2TP_USER_NAME}
+password ${L2TP_PASSWORD}
 EOS
