a2e3c5b63a20dd83f00ea933969d9348d2ec86d3
Metallion <akieckens@axsh.net>
2017-04-05 16:06:13 +0900

    Automatically figure out which interface we need to run the vpn on

--- start_vpn.sh
+++ start_vpn.sh
@@ -9,13 +9,18 @@ fi
 #VPN_SERVER_IP
 #TARGET_IP=
 
-LOCAL_INTERFACE="${LOCAL_INTERFACE:-eth0}"
 TUNNEL_INTERFACE="${TUNNEL_INTERFACE:-ppp0}"
 CONNECTION_NAME="${CONNECTION_NAME:-L2TP-PSK}"
+
+function get_default_gw_interface() {
+  ip route show | grep default | grep -oP "(?<=dev )[^ ]+"
+}
 
 function get_tunnel_ip() {
   ip addr show $TUNNEL_INTERFACE | grep "inet\b" | awk '{print $2}'
 }
+
+LOCAL_INTERFACE="${LOCAL_INTERFACE:-$(get_default_gw_interface)}"
 
 #ip route add $TARGET_IP via $(get_tunnel_ip)
 
